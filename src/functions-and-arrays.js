b1ceeb900c5462684a10a925758b41b2159a07e1
Elisa Exposito <eliexme@gmail.com>
2023-03-30 14:44:22 +0100

    Done up to 4 iteration

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,21 +1,55 @@
 // Iteration #1: Find the maximum
-function maxOfTwoNumbers() {}
+function maxOfTwoNumbers(a,b) {
+  if(a>=b){
+    return a
+  }
+  else {
+    return b
+  }
+}
 
 
 
 // Iteration #2: Find longest word
 const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
 
-function findLongestWord() {}
+function findLongestWord(words) {
+  if (words.length === 0){
+    return null
+  }
+  else if (words.length === 1){
+    return words[0]
+  }
+  else{
+    let longestWord = words[0]
+    for (let i=0; i<words.length; i++){
+      if (words[i].length > longestWord.length){
+        longestWord = words [i]
+      }
+      
+    }
+    return longestWord
+  }
+}
 
 
 
 // Iteration #3: Calculate the sum
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
-
-
+function sumNumbers(numbers) {
+  if (numbers.length === 0){
+    return 0
+  }
+  else{
+    let sum = 0
+    for (let i=0; i<numbers.length; i++){
+    sum += numbers[i]}
+    return sum
+  }
+  
+}
+ 
 
 // Iteration #3.1 Bonus:
 function sum() {}
@@ -128,3 +162,4 @@ if (typeof module !== 'undefined') {
     greatestProduct
   };
 }
+
